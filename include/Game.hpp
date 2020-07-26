7c971027d1092ee1c93b35f43e26882bf6fbc259
ananas <lucienfiorini@protonmail.com>
2020-07-26 09:39:36 +0200

    Fix: Tab lenght

--- include/Game.hpp
+++ include/Game.hpp
@@ -5,31 +5,31 @@
 
 class Game
 {
-        public:
-                Game();
-                ~Game();
+    public:
+        Game();
+        ~Game();
 
-                void Init(const char *title, int xpos, int ypos, int width, int height,
-                          bool fullscreen);
+        void Init(const char *title, int xpos, int ypos, int width, int height,
+                  bool fullscreen);
 
-                void SetFps(const int FPS);
-                void StartFrame();
-                void EndFrame();
+        void SetFps(const int FPS);
+        void StartFrame();
+        void EndFrame();
 
-                void HandleEvent();
-                void Update();
-                void Render();
-                void Clean();
-                bool Running() { return isRunning_; }
+        void HandleEvent();
+        void Update();
+        void Render();
+        void Clean();
+        bool Running() { return isRunning_; }
 
-                static SDL_Renderer *renderer;
-                static SDL_Event event;
+        static SDL_Renderer *renderer;
+        static SDL_Event event;
 
-        private:
-                bool isRunning_;
-                SDL_Window *window_;
-                int frameDelay_;
-                int frameTime_;
-                Uint32 frameStart_;
-                int frameCount_;
+    private:
+        bool isRunning_;
+        SDL_Window *window_;
+        int frameDelay_;
+        int frameTime_;
+        Uint32 frameStart_;
+        int frameCount_;
 };
